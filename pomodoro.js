3cd300887512e30b80aa39f240e7ce462e7c10a0
mellacid <melisa.bcd@icloud.com>
2023-08-31 15:41:42 +0200

    added pause and continue

--- pomodoro.js
+++ pomodoro.js
@@ -1,8 +1,11 @@
 const timerDisplay = document.getElementById("timer");
 const startButton = document.getElementById("startButton");
 const resetButton = document.getElementById("resetButton");
-let timeLeft = 1500; // 25 minutes in seconds
+const pauseButton = document.getElementById("pauseButton");
+const continueButton = document.getElementById("continueButton");
+let timeLeft = 60; // 25 minutes in seconds
 let timerInterval;
+let isPaused = false;
 
 function updateTimer() {
   const minutes = Math.floor(timeLeft / 60);
@@ -13,17 +16,29 @@ function updateTimer() {
 function startTimer() {
   clearInterval(timerInterval);
   timerInterval = setInterval(() => {
-    if (timeLeft > 0) {
-      timeLeft--;
-      updateTimer();
-    } else {
-      clearInterval(timerInterval);
-      playSound();
-      showNotification("Arbeitsintervall abgeschlossen. Zeit für eine Pause!");
-      timeLeft = 300; // 5 minutes break
-      updateTimer();
+    if (!isPaused) {
+      if (timeLeft > 0) {
+        timeLeft--;
+        updateTimer();
+      } else {
+        clearInterval(timerInterval);
+        playSound();
+        showNotification(
+          "Arbeitsintervall abgeschlossen. Zeit für eine Pause!"
+        );
+        timeLeft = 300; // 5 minutes break
+        updateTimer();
+      }
     }
   }, 1000);
+}
+
+function pauseTimer() {
+  isPaused = true;
+}
+
+function continueTimer() {
+  isPaused = false;
 }
 
 function resetTimer() {
@@ -38,6 +53,14 @@ function playSound() {
 
 startButton.addEventListener("click", () => {
   startTimer();
+});
+
+pauseButton.addEventListener("click", () => {
+  pauseTimer();
+});
+
+continueButton.addEventListener("click", () => {
+  continueTimer();
 });
 
 resetButton.addEventListener("click", resetTimer);
