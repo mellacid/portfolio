a3ba191912aa044607eb24a21beb749b00f9c880
mellacid <melisa.bcd@icloud.com>
2023-09-04 10:47:04 +0200

    explaining js

--- pomodoro.js
+++ pomodoro.js
@@ -1,3 +1,4 @@
+// Zuerst werden alle wichtigen HTML-Elemente durch ihre IDs aus dem DOM (Document Object Model) geholt.
 const timerDisplay = document.getElementById("timer");
 const startButton = document.getElementById("startButton");
 const resetButton = document.getElementById("resetButton");
@@ -5,95 +6,106 @@ const pauseButton = document.getElementById("pauseButton");
 const continueButton = document.getElementById("continueButton");
 const messageDisplay = document.getElementById("message");
 
-let pomodoroCount = 0;
-let timeLeft = 1500; // 25 Minuten in Sekunden (Standard-Pomodoro)
-let timerInterval;
-let isPaused = false;
-let originalMessage = "Pomodoro-Modus: 25 Minuten konzentriert arbeiten!"; // Speichern der ursprünglichen Nachricht
+// Initialisierung von Variablen für den Timer und den Status
+let pomodoroCount = 0; // Ein Zähler für abgeschlossene Pomodoro-Einheiten
+let timeLeft = 1500; // Die verbleibende Zeit in Sekunden (Standard-Pomodoro: 25 Minuten)
+let timerInterval; // Eine Variable zur Speicherung des Timer-Intervalls
+let isPaused = false; // Ein Status, um zu überprüfen, ob der Timer pausiert ist
+let originalMessage = "Pomodoro-Modus: 25 Minuten konzentriert arbeiten!"; // Die ursprüngliche Nachricht speichern
 
+// Eine Funktion zum Aktualisieren der Timer-Anzeige (Minuten und Sekunden)
 function updateTimer() {
   const minutes = Math.floor(timeLeft / 60);
   const seconds = timeLeft % 60;
   timerDisplay.textContent = `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 }
 
+// Eine Funktion zum Anzeigen von Nachrichten im Timer
 function showMessage(message) {
   messageDisplay.textContent = message;
 }
 
+// Die Funktion zum Starten des Timers
 function startTimer() {
-  clearInterval(timerInterval);
+  clearInterval(timerInterval); // Zuerst wird das aktuelle Timer-Intervall gelöscht
   timerInterval = setInterval(() => {
+    // Dann wird ein neues Intervall gestartet
     if (!isPaused) {
+      // Der Timer läuft nur, wenn er nicht pausiert ist
       if (timeLeft > 0) {
         timeLeft--;
         updateTimer();
       } else {
-        clearInterval(timerInterval);
-        playSound();
-        pomodoroCount++;
+        // Wenn die Zeit abgelaufen ist
+        clearInterval(timerInterval); // Das Intervall wird gelöscht
+        playSound(); // Hier könnte man Code zum Abspielen eines Sounds einfügen
+        pomodoroCount++; // Der Pomodoro-Zähler wird erhöht
         if (pomodoroCount % 4 === 0) {
-          showMessage("Große Pause. Zeit für eine Pause!");
-          timeLeft = 1500; // 25 Minuten für die große Pause
+          // Wenn ein Pomodoro-Zyklus abgeschlossen ist
+          showMessage("Große Pause. Zeit für eine Pause!"); // Die Nachricht für die große Pause wird angezeigt
+          timeLeft = 1500; // Die Zeit wird auf 25 Minuten für die große Pause zurückgesetzt
         } else {
-          showMessage("Arbeitsintervall abgeschlossen. Zeit für eine Pause!");
-          timeLeft = 300; // 5 Minuten Pause
+          // Wenn ein Arbeitsintervall abgeschlossen ist
+          showMessage("Arbeitsintervall abgeschlossen. Zeit für eine Pause!"); // Die Nachricht für die Pomodoro-Pause wird angezeigt
+          timeLeft = 300; // Die Zeit wird auf 5 Minuten für die Pause zurückgesetzt
         }
-        updateTimer();
-        startTimer();
+        updateTimer(); // Die Timer-Anzeige wird aktualisiert
+        startTimer(); // Der Timer wird erneut gestartet, um die Pause zu beginnen
       }
     }
-  }, 1000);
+  }, 1000); // Das Intervall wird alle 1000 Millisekunden (1 Sekunde) aktualisiert
 }
 
+// Die Funktion zum Pausieren des Timers
 function pauseTimer() {
-  isPaused = true;
-  showMessage("Pause läuft...");
+  isPaused = true; // Der Status wird auf pausiert gesetzt
+  showMessage("Pause läuft..."); // Die Nachricht "Pause läuft..." wird angezeigt
 }
 
+// Die Funktion zum Fortsetzen des Timers
 function continueTimer() {
-  isPaused = false;
+  isPaused = false; // Der Status wird auf nicht pausiert gesetzt
   if (pomodoroCount % 4 === 0) {
-    showMessage(originalMessage); // Zeige die ursprüngliche Nachricht für Große Pause
+    showMessage(originalMessage); // Wenn es eine große Pause ist, wird die ursprüngliche Nachricht angezeigt
   } else {
-    showMessage("Arbeitsintervall abgeschlossen. Zeit für eine Pause!"); // Zeige die Nachricht für Pomodoro-Pause
+    showMessage("Arbeitsintervall abgeschlossen. Zeit für eine Pause!"); // Ansonsten wird die Nachricht für das Pomodoro-Intervall angezeigt
   }
 }
 
+// Die Funktion zum Zurücksetzen des Timers
 function resetTimer() {
-  clearInterval(timerInterval);
-  pomodoroCount = 0;
-  timeLeft = 1500; // Zurücksetzen auf 25 Minuten (Standard-Pomodoro)
-  updateTimer();
-  isPaused = false;
-  showMessage("");
-  startButton.style.display = "inline"; // Zeige den Start-Button
-  continueButton.style.display = "inline"; // Zeige den Weiter-Button
-  pauseButton.style.display = "inline"; // Zeige den Pause-Button
+  clearInterval(timerInterval); // Das Timer-Intervall wird gelöscht
+  pomodoroCount = 0; // Der Pomodoro-Zähler wird zurückgesetzt
+  timeLeft = 1500; // Die Zeit wird auf 25 Minuten (Standard-Pomodoro) zurückgesetzt
+  updateTimer(); // Die Timer-Anzeige wird aktualisiert
+  isPaused = false; // Der Status wird auf nicht pausiert gesetzt
+  showMessage(""); // Die Nachricht wird gelöscht
+  startButton.style.display = "inline"; // Der Start-Button wird angezeigt
+  continueButton.style.display = "inline"; // Der Weiter-Button wird angezeigt
+  pauseButton.style.display = "inline"; // Der Pause-Button wird angezeigt
 }
 
-function playSound() {
-  // Hier kannst du den Code zum Abspielen eines Sounds einfügen
-}
+// Eine Funktion zum Abspielen von Sounds könnte hier ergänzt werden
 
+// Event-Listener für die Buttons
 startButton.addEventListener("click", () => {
-  startTimer();
-  showMessage(originalMessage);
-  startButton.style.display = "none"; // Verstecke den Start-Button
-  continueButton.style.display = "none"; // Verstecke den Weiter-Button
-  pauseButton.style.display = "inline"; // Zeige den Pause-Button
+  startTimer(); // Der Timer wird gestartet
+  showMessage(originalMessage); // Die ursprüngliche Nachricht wird angezeigt
+  startButton.style.display = "none"; // Der Start-Button wird versteckt
+  continueButton.style.display = "none"; // Der Weiter-Button wird versteckt
+  pauseButton.style.display = "inline"; // Der Pause-Button wird angezeigt
 });
 
 pauseButton.addEventListener("click", () => {
-  pauseTimer();
-  pauseButton.style.display = "none"; // Verstecke den Pause-Button
-  continueButton.style.display = "inline"; // Zeige den Weiter-Button
+  pauseTimer(); // Der Timer wird pausiert
+  pauseButton.style.display = "none"; // Der Pause-Button wird versteckt
+  continueButton.style.display = "inline"; // Der Weiter-Button wird angezeigt
 });
 
 continueButton.addEventListener("click", () => {
-  continueTimer();
-  continueButton.style.display = "none"; // Verstecke den Weiter-Button
-  pauseButton.style.display = "inline"; // Zeige den Pause-Button
+  continueTimer(); // Der Timer wird fortgesetzt
+  continueButton.style.display = "none"; // Der Weiter-Button wird versteckt
+  pauseButton.style.display = "inline"; // Der Pause-Button wird angezeigt
 });
 
-resetButton.addEventListener("click", resetTimer);
+resetButton.addEventListener("click", resetTimer); // Der Timer wird zurückgesetzt
